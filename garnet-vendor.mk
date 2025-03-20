d63a8f6034a629fc9af8afe5cddfb1518b2ac4c5
Arian <arian.kulmer@web.de>
2025-03-19 21:10:57 -0300

    garnet: Unpin telephony apks
    Move QtiTelephony to priv-app directory for proper placement and permissions
    
    Change-Id: I86d82dbe05c5ea2bc780d897c1e9c74deba13b32

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1932,8 +1932,8 @@ PRODUCT_PACKAGES += \
     ImsRcsService \
     PowerOffAlarm \
     QCC \
-    QtiTelephony \
     QtiTelephonyService \
+    QtiTelephony \
     WfdService \
     ims \
     qcrilmsgtunnel \
